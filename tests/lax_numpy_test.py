74b70ef1d9c339ba0f4f81d76564816be0879ae7
Daniel Suo <dsuo@google.com>
2026-02-23 09:56:12 -0800

    [lax_numpy] Adjust tolerance for np.float32 in trapezoid tests.
    Fix flaky tests on H100 and B200 variants of the lax_numpy_tests.
    
    PiperOrigin-RevId: 874126004

--- tests/lax_numpy_test.py
+++ tests/lax_numpy_test.py
@@ -6006,8 +6006,15 @@ class LaxBackedNumpyTests(jtu.JaxTestCase):
     args_maker = lambda: [rng(yshape, dtype), rng(xshape, dtype) if xshape is not None else None]
     np_fun = partial(np.trapezoid, dx=dx, axis=axis)
     jnp_fun = partial(jnp.trapezoid, dx=dx, axis=axis)
-    tol = jtu.tolerance(dtype, {np.float16: 4e-3, np.float64: 1e-12,
-                                jax.dtypes.bfloat16: 4e-2})
+    tol = jtu.tolerance(
+        dtype,
+        {
+            np.float16: 4e-3,
+            np.float32: 5e-6,
+            np.float64: 1e-12,
+            jax.dtypes.bfloat16: 4e-2,
+        },
+    )
     self._CheckAgainstNumpy(np_fun, jnp_fun, args_maker, tol=tol,
                             check_dtypes=False)
     self._CompileAndCheck(jnp_fun, args_maker, atol=tol, rtol=tol,
